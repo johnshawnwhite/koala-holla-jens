b5a9c782eb44ed7e556b43e7951fab7c76575ccc
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-07-02 10:13:49 -0400

    client side post and get

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -16,25 +16,88 @@ function setupClickListeners() {
     // NOT WORKING YET :(
     // using a test object
     let koalaToSend = {
-      name: 'testName',
-      age: 'testName',
-      gender: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+       koalaName: $('#koalaName').val(),
+       koalaAge: $('#koalaAge').val(),
+       koalaGender: $('#koalaGender').val(),
+       readyToTransfer: $('#readyToTransfer').val(),
+       koalaNote: $('#koalaNote').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( koalaToSend );
   }); 
 }
-
-function getKoalas(){
-  console.log( 'in getKoalas' );
-  // ajax call to server to get koalas
+//Post new Koala to the Server
+function addKoala() {
   
-} // end getKoalas
+  $.ajax({
+    method: 'POST', 
+    url: '/koalas', 
+    data: {
+      koalaToSend = {
+        koalaName: $('#koalaName').val(),
+        koalaAge: $('#koalaAge').val(),
+        koalaGender: $('#koalaGender').val(),
+        readyToTransfer: $('#readyToTransfer').val(),
+        koalaNote: $('#koalaNote').val(),
+      }
+    }
+  })
+  .then(function (response) {
+    console.log('Claws!!');
+  })
+  .catch(function (error) {
+    alert('request failed');
+  })
+}
 
-function saveKoala( newKoala ){
-  console.log( 'in saveKoala', newKoala );
-  // ajax call to server to get koalas
- 
+
+// send koalas list to the dom
+function getKoalas() {
+  console.log( 'in getKoalas' );
+  $.ajax({
+    method: 'GET',
+    url: '/koalas',
+  })
+    .then(result => {
+      console.log('GET /koalas', result);
+
+        
+      $('#viewKoalas').empty();
+        for (let koalaToSend of result) {
+          $('#viewKoalas').append(
+            <tr>
+        <td>${koalaToSend.koalaName}</td> 
+        <td>${koalaToSend.koalaAge} </td>
+        <td>${koalaToSend.koalaGender}</td> 
+        <td>${koalaToSend.readyToTransfer}</td> 
+        <td>${koalaToSend.koalaNote}</td>
+        <td></td>
+        <td></td>
+          </tr>
+          );
+      }
+
+
+    })
+    .catch(err => {
+      console.log(' GET /koalas', err);
+
+
+      
+      
+    })
+}
+
+
+function saveKoala() {
+  $.ajax({
+    method: 'GET',
+    url: '/koalas'
+  })
+  .then(function (response) { 
+    console.log('Claws!!', response);
+  })
+  .catch(function (error) { 
+    console.log('Error!', error)
+  });
 }
