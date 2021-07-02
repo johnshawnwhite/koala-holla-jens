81c8a7e69ff56e3b20ecabe84e65754d82b83371
Abram Chavez <abram.chavez820@gmail.com>
2021-07-01 20:27:53 -0500

    created pool.js w/ const pg and port

---
+++ server/public/scripts/pool.js
@@ -0,0 +1,10 @@
+const pg =require('pg');
+const Pool =pg.Pool;
+const pool = new Pool ({
+    database: 'koalas',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis:
+    30000
+});
