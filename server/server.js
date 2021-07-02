41c45b97c33c75e536f9cae06fa8c24aa62cf1f8
Kong Yang <kpyang2414@gmail.com>
2021-07-01 20:37:05 -0500

    started basic app

--- server/server.js
+++ server/server.js
@@ -3,12 +3,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const koalaRouter = require('./routes/koala.router')
+const pg = require('pg');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // ROUTES
-app.use('/koalas', koalaRouter)
+app.use('/koalas', koalaRouter);
+//-----------required server side code above----------------//
+
+app.get('/koalas', (req, res) => {
+  console.log('Inside app.get', req);
+})
+app.post('/koalas', (req, res) => {
+  console.log('Inside app.post', req);
+})
+app.put('/:id', (req, res) => {
+  console.log('inside app.put', req.params.id);
+})
+app.delete('/:id', (req, res) => {
+  console.log('inside delete', req.url);
+})
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
